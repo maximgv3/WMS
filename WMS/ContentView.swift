6b7cce2542f6c476945b11388e9bdc069e6fd711
Maxim <mquesmark@icloud.com>
2026-04-23 20:55:31 +0300

    Initial Commit

---
+++ WMS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WMS
+//
+//  Created by Максим on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WMS/WMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WMSApp.swift
+//  WMS
+//
+//  Created by Максим on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct WMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
