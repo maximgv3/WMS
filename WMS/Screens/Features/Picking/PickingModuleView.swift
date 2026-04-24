c32441bbf187c714c5d1cbf7fb0dacdc636bd566
Maxim <mquesmark@icloud.com>
2026-04-24 23:03:22 +0300

    [Feature] Add test basic Picking Module layout

--- WMS/Screens/Features/Picking/PickingModuleView.swift
+++ WMS/Screens/Features/Picking/PickingModuleView.swift
@@ -2,6 +2,28 @@ import SwiftUI
 
 struct PickingModuleView: View {
     var body: some View {
-        Text("Picking Module View")
+        VStack(spacing: 32) {
+            Image(systemName: "arrow.down.circle")
+                .foregroundStyle(ColorPalette.brandPrimary)
+                .font(.system(size: 64))
+            Button("Получить задание") {
+                getTaskTapped()
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            .tint(ColorPalette.accentPrimary)
+            .foregroundStyle(ColorPalette.brandPrimary)
+            .font(.system(size: 20, weight: .semibold))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(ColorPalette.backgroundPrimary)
     }
+
+    private func getTaskTapped() {
+
+    }
+}
+
+#Preview {
+    PickingModuleView()
 }
