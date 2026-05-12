f21f32edc7b6f87d31cc19743d1332cdc7bfe7c6
Maxim <mquesmark@icloud.com>
2026-05-12 22:08:47 +0300

    [Feature] Add TabView in ContentView, add ProfileView draft

--- WMS/ContentView.swift
+++ WMS/ContentView.swift
@@ -2,11 +2,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        OperationsListView()
-            .preferredColorScheme(.light)
+        TabView {
+            OperationsListView()
+                .tint(ColorPalette.brandPrimary)
+                .tabItem {
+                    Label("Операции", systemImage: "shippingbox")
+                }
+
+            ProfileView()
+                .tint(ColorPalette.brandPrimary)
+                .tabItem {
+                    Label("Профиль", systemImage: "person.crop.circle")
+                }
+        }
+        .tint(ColorPalette.accentPrimary)
+        .preferredColorScheme(.light)
     }
 }
-
 #Preview {
     ContentView()
 }
