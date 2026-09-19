fc475e0042e33dc6c806ff638e1f6c8c18f0af14
Maxim <mquesmark@icloud.com>
2026-09-19 16:26:34 +0300

    polish(glass): add isActive and isDimmed parameters

--- WMS/Utilities/View+GlassIfAvailable.swift
+++ WMS/Utilities/View+GlassIfAvailable.swift
@@ -3,13 +3,27 @@ import SwiftUI
 extension View {
     @ViewBuilder
     func glassIfAvailable(
+        _ isActive: Bool = true,
         shape: some Shape = Capsule(),
-        background: some ShapeStyle = Material.ultraThinMaterial
+        background: some ShapeStyle = Material.ultraThinMaterial,
+        isDimmed: Bool = false
     ) -> some View {
-        if #available(iOS 26, *) {
-            self.glassEffect(.regular.interactive(), in: shape)
+        if isActive {
+            if #available(iOS 26, *) {
+                self
+                    .overlay {
+                        if isDimmed {
+                            shape
+                                .fill(.black.opacity(0.05))
+                                .allowsHitTesting(false)
+                        }
+                    }
+                    .glassEffect(.regular.interactive(), in: shape)
+            } else {
+                self.background(background, in: shape)
+            }
         } else {
-            self.background(background, in: shape)
+            self
         }
     }
 }
